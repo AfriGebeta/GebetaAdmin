52281502f6e6305e921319d4ce91569a987a6ce9
Amanuel Abebe <amanuelabebe2000@gmail.com>
2023-07-22 20:26:52 +0300

    modified the sidebar buttons and added active and focus states to make it look like it's clicking

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -20,33 +20,33 @@ const SideBar = () => {
             </div>
             
 
-            <div className='md:bg-secondary bg-shadedGrey pl-4 overflow-hidden rounded-[10px]'>
+            <div className='bg-secondary bg-shadedGrey pl-4 overflow-hidden rounded-[10px]'>
                 <ul className='flex flex-col gap-2'>
                     {/* Home */}
                     <li>
-                        <a href="#" className='flex gap-2 hover:text-primary'>
-                            <img src={Home} alt="" className='w-[25px] h-[25px] hover:scale-105'/>
+                        <a href="#" className='flex gap-2 hover:text-primary hover:scale-105 active:scale-90 focus:scale-90 focus:text-primary'>
+                            <img src={Home} alt="" className='w-[25px] h-[25px]'/>
                             <p className='hidden sm:flex'>Home</p>
                         </a>
                     </li>
                     {/* Client Managment */}
                     <li>
-                        <a href="#" className='flex gap-2 hover:text-primary'>
-                            <img src={ClientMgmt} alt="" className='w-[25px] h-[25px] hover:scale-105' />
+                        <a href="#" className='flex gap-2 hover:text-primary hover:scale-105 active:scale-90 focus:scale-90 focus:text-primary'>
+                            <img src={ClientMgmt} alt="" className='w-[25px] h-[25px]' />
                             <p className='hidden sm:flex'>Client Managment</p>
                         </a>
                     </li>
                     {/* API Keys */}
                     <li>
-                        <a href="#" className='flex gap-2 hover:text-primary'>
-                            <img src={ApiKey} alt="" className='w-[25px] h-[25px] hover:scale-105' />
+                        <a href="#" className='flex gap-2 hover:text-primary hover:scale-105 active:scale-90 focus:scale-90 focus:text-primary'>
+                            <img src={ApiKey} alt="" className='w-[25px] h-[25px]' />
                             <p className='hidden sm:flex'>API Keys</p>
                         </a>
                     </li>
                     {/* NewsLetter */}
                     <li>
-                         <a href="#" className='flex gap-2 hover:text-primary'>
-                            <img src={Newsletter} alt="" className='w-[25px] h-[25px] hover:scale-105' />
+                         <a href="#" className='flex gap-2 hover:text-primary hover:scale-105 active:scale-90 focus:scale-90 focus:text-primary'>
+                            <img src={Newsletter} alt="" className='w-[25px] h-[25px]' />
                             <p className='hidden sm:flex'>Newsletter</p>
                         </a>
                     </li>
@@ -63,26 +63,26 @@ const SideBar = () => {
                 <p className='hidden sm:flex'>Projects</p>
             </div>
 
-            <div className='md:bg-secondary bg-shadedGrey pl-4 overflow-hidden rounded-[10px]'>
+            <div className='bg-secondary bg-shadedGrey pl-4 overflow-hidden rounded-[10px]'>
                 <ul className='flex flex-col gap-2'>
                     {/* Ongoing Projects */}
                     <li>
-                        <a href="#" className='flex gap-2 hover:text-primary'>
-                            <img src={OngoingProjects} alt="" className='w-[25px] h-[25px] hover:scale-105' />
+                        <a href="#" className='flex gap-2 hover:text-primary hover:scale-105 active:scale-90 focus:scale-90 focus:text-primary'>
+                            <img src={OngoingProjects} alt="" className='w-[25px] h-[25px]' />
                             <p className='hidden sm:flex'>Ongoing Projects</p>
                         </a>
                     </li>
                     {/* Meetings */}
                     <li>
-                        <a href="#" className='flex gap-2 hover:text-primary'>
-                            <img src={Meetings} alt="" className='w-[25px] h-[25px] hover:scale-105' />
+                        <a href="#" className='flex gap-2 hover:text-primary hover:scale-105 active:scale-90 focus:scale-90 focus:text-primary'>
+                            <img src={Meetings} alt="" className='w-[25px] h-[25px]' />
                             <p className='hidden sm:flex'>Meetings</p>
                         </a>
                     </li>
                     {/* Basket */}
                     <li>
-                        <a href="#" className='flex gap-2 hover:text-primary'>
-                            <img src={Basket} alt="" className='w-[25px] h-[25px] hover:scale-105' />
+                        <a href="#" className='flex gap-2 hover:text-primary hover:scale-105 active:scale-90 focus:scale-90 focus:text-primary'>
+                            <img src={Basket} alt="" className='w-[25px] h-[25px]' />
                             <p className='hidden sm:flex'>Basket</p>
                         </a>
                     </li>
@@ -92,8 +92,8 @@ const SideBar = () => {
         </div>
 
         <div className='bottom-6 fixed'>
-            <a href="#" className='flex gap-2 hover:text-primary'>
-                <img src={Logout} alt="" className='w-[30px] h-[30px] hover:scale-105' />
+            <a href="#" className='flex gap-2 hover:text-primary hover:scale-105 active:scale-90 focus:scale-90 focus:text-primary'>
+                <img src={Logout} alt="" className='w-[30px] h-[30px]' />
                 <p className='hidden sm:flex'>Logout</p>
             </a>
         </div>
