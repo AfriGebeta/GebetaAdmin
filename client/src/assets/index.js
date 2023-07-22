ea890a1ddfb3f109bed82c407dfaa6b56c2b98f9
Amanuel Abebe <amanuelabebe2000@gmail.com>
2023-07-22 20:04:49 +0300

    Sidebar Complete

---
+++ client/src/assets/index.js
@@ -0,0 +1,26 @@
+import Logo from "./gebetalogo.png"
+import Clients from "./clients-logo.svg"
+import Projects from "./projects-logo.svg"
+import Logout from "./logout.svg"
+import ApiKey from "./api-key-logo.svg"
+import Basket from "./basket-logo.svg"
+import ClientMgmt from "./client-mgmt-logo.svg"
+import Meetings from "./meetings-logo.png"
+import Newsletter from "./newsletter-logo.svg"
+import OngoingProjects from "./ongoing-projects-logo.svg"
+import Home from "./home-logo.svg"
+
+
+export {
+    Logo,
+    Clients,
+    Projects,
+    Logout,
+    ApiKey,
+    Basket,
+    ClientMgmt,
+    Meetings,
+    Newsletter,
+    OngoingProjects,
+    Home
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffffff",
+        secondary: "#d3d3d3",
+        shadedGrey: "#DFDFDF"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
