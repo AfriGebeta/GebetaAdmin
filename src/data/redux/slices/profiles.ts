7043521f1b58305a61baf9293d4850b345fb16ac
Zubeyr Anwar <zubeyranwar@gmail.com>
2024-08-06 08:42:31 -0400

    fix: FIxing duplicate import

--- src/data/redux/slices/profiles.ts
+++ src/data/redux/slices/profiles.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '../store'
-import { Boundary, Profile } from '@/model'
+import { Profile } from '@/model'
 import { arrayToHashMap } from '@/utils'
 
 const NAME = 'profiles'
