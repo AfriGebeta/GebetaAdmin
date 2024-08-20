cb2d9632712e7f4711bd48efa0f732510f0b2261
zubeyranwar <zubeyranwar@gmail.com>
2024-08-20 19:29:36 +0300

    feat:Integrating edit place

--- src/pages/places/components/columns.tsx
+++ src/pages/places/components/columns.tsx
@@ -28,6 +28,7 @@ import {
 } from '@/components/ui/carousel.tsx'
 import { Badge } from '@/components/ui/badge.tsx'
 import { Link } from 'react-router-dom'
+import { Checkbox } from '@/components/ui/checkbox.tsx'
 
 export const columns: ColumnDef<{
   id: string
@@ -43,30 +44,30 @@ export const columns: ColumnDef<{
   address: Address
   contact: Contact
 }>[] = [
-  // {
-  //   id: 'select',
-  //   header: ({ table }) => (
-  //     <Checkbox
-  //       checked={
-  //         table.getIsAllPageRowsSelected() ||
-  //         (table.getIsSomePageRowsSelected() && 'indeterminate')
-  //       }
-  //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-  //       aria-label='Select all'
-  //       className='translate-y-[2px]'
-  //     />
-  //   ),
-  //   cell: ({ row }) => (
-  //     <Checkbox
-  //       checked={row.getIsSelected()}
-  //       onCheckedChange={(value) => row.toggleSelected(!!value)}
-  //       aria-label='Select row'
-  //       className='translate-y-[2px]'
-  //     />
-  //   ),
-  //   enableSorting: false,
-  //   enableHiding: false,
-  // },
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && 'indeterminate')
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label='Select all'
+        className='translate-y-[2px]'
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label='Select row'
+        className='translate-y-[2px]'
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
   {
     accessorKey: 'name',
     header: ({ column }) => (
