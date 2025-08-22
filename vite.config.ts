f5d195d9c89304461fcd9a9aabd0ba1e9474f612
Kirubel <kirubeldesalegn17@gmail.com>
2025-08-22 17:10:17 +0300

    added users filtering by credit, updated getUsers, searchUsers and handled CORS issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const apiBase = env.VITE_API_BASE_URL
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: apiBase,
+          changeOrigin: true,
+          secure: true,
+        },
+      },
+    },
+  }
 })
